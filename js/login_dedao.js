d0e7f1c4f5a0304b9661dea654e2aae1ab577a71
hj <xmhj0220@163.com>
2023-05-20 13:20:26 +0800

    completed

--- js/login_dedao.js
+++ js/login_dedao.js
@@ -1,12 +1,22 @@
+/* 脚本：实现得到页面自动输入手机号、获取验证码，避免频繁输入 */
 (function () {
     'use strict';
 
     // Your code here...
     document.querySelector('div.login-btn').click();
-    document.querySelector('input[name="phone-number"]').value = '18079014431';
-    document.querySelector('div.dd-checkBox').click();
-    document.querySelector('label[for="auth-code"]').click();
-    document.querySelector('input[name="auth-code"]').focus();
-    console.log('load complete');
-
+    setTimeout(() => {
+        // code to execute after 2 seconds
+        document.querySelector('div.dd-checkBox').click();
+        const phoneNumberInput = document.querySelector('input[name="phone-number"]');
+        if (phoneNumberInput !== null) {
+            phoneNumberInput.value = '【手机号】';
+            var event = document.createEvent('HTMLEvents');
+            event.initEvent("input", true, true);
+            event.eventType = 'message';
+            phoneNumberInput.dispatchEvent(event);
+        }
+        document.querySelector('label[for="auth-code"]').click();
+        document.querySelector('input[name="auth-code"]').focus();
+        console.log('load complete');
+    }, 500);
 })();
